ca276398243810d5239e4ebcefc80d8b2f0e970b
Hackbright Student <info@hackbrightacademy.com>
2014-06-12 18:06:13 -0700

    end of the day, we're done

--- core.py
+++ core.py
@@ -1,3 +1,7 @@
+GAME_WIDTH = 5
+GAME_HEIGHT = 5
+
+
 class GameElement(object):
     IMAGE = "StoneBlock"
     SOLID = False
@@ -8,11 +12,25 @@ class GameElement(object):
         self.x = None
         self.y = None
 
-    def interact(self, player):
-        pass
+    def interact(self, direction):
+        return (self.x, self.y)
 
     def __str__(self):
         return "<%s located at %r, %r>"%(type(self).__name__, self.x, self.y)
 
     def update(self, dt):
         pass
+
+    def level_up(self, x, y):
+        pass
+
+    def move_forward(self, direction):
+        if direction == "up" and self.y-1 >= 0:
+            return (self.x, self.y-1)
+        elif direction == "down" and self.y+1 < GAME_HEIGHT:
+            return (self.x, self.y+1)
+        elif direction == "left" and self.x-1 >= 0:
+            return (self.x-1, self.y) 
+        elif direction == "right" and self.x+1 < GAME_WIDTH:
+            return (self.x+1, self.y)
+        return (self.x, self.y)
--- game.py
+++ game.py
@@ -3,113 +3,139 @@ import pyglet
 from pyglet.window import key
 from core import GameElement
 import sys
+from random import choice, randint
 
 #### DO NOT TOUCH ####
 GAME_BOARD = None
 DEBUG = False
 KEYBOARD = None
 PLAYER = None
+CURRENT_LEVEL = None
 ######################
 
 GAME_WIDTH = 5
 GAME_HEIGHT = 5
 
-######################
-LEVEL1 = {
-    (0, 0): '',
-    (0, 1): '',
-    (0, 2): '',
-    (0, 3): '',
-    (0, 4): '',
-    (1, 0): '',
-    (1, 1): '',
-    (1, 2): 'Gem',
-    (1, 3): '',
-    (1, 4): '',
-    (2, 0): '',
-    (2, 1): '',
-    (2, 2): '',
-    (2, 3): 'Gem',
-    (2, 4): '',
-    (4, 0): 'Chest'
-}
-
-LEVEL2 = {
-    (0, 0): 'BlueGem',
-    (0, 1): 'BlueGem',
-    (0, 2): 'Gem',
-    (1, 0): '',
-    (1, 1): '',
-    (1, 2): 'BlueGem',
-    (2, 0): 'BlueGem',
-    (2, 1): '',
-    (2, 2): 'BlueGem'
-}
+#####################
 
 #### Put class definitions here ####
+class Level(object):
+    current_level = 1
+    new_level= {}
+    chest_location = None
+
+    def make_next_level(self):
+        #recreate the level_dict with empty values
+        for x in range(GAME_WIDTH):
+            for y in range(GAME_HEIGHT):
+                if GAME_BOARD.get_el(x, y) == PLAYER:
+                    pass
+                else:
+                    self.new_level[(x,y)] = ''
+
+        # add the chest
+        chest = choice(self.new_level.keys())
+        self.chest_location = chest
+
+        self.new_level[chest] = 'Chest'
+        #add the green gem not on the chest or the edge
+
+        while True:
+            rand_x = randint(1, GAME_WIDTH - 2)
+            rand_y = randint(1, GAME_HEIGHT - 2)
+
+            if chest != (rand_x, rand_y):
+                self.new_level[(rand_x, rand_y)] = 'Gem'
+                gem_location = (rand_x, rand_y)
+                break
+            else:
+                pass
+
+        print chest, gem_location        
+        #add random trees and rocks                
+        num_objects = 0
+        while num_objects < 4:
+            extra_object = choice(['Rock','Tree'])
+            rand_x = randint(0, GAME_WIDTH - 1)
+            rand_y = randint(0, GAME_HEIGHT - 1)
+            if chest != (rand_x, rand_y) and gem_location != (rand_x, rand_y):
+                self.new_level[(rand_x, rand_y)] = extra_object
+                num_objects += 1
+            else:
+                pass    
+
+        return self.new_level        
+
 class Rock(GameElement):
     IMAGE = "Rock"
     SOLID = True
 
+    def move_forward(self, direction):
+        return (self.x, self.y)
+
 class Character(GameElement):
     IMAGE = "Horns"
 
-    def __init__(self):
-        GameElement.__init__(self)
-        self.inventory = []
-
-    def next_pos(self, direction):
-        if direction == "up" and self.y-1 >= 0:
-            return (self.x, self.y-1)
-        elif direction == "down" and self.y+1 < GAME_HEIGHT:
-            return (self.x, self.y+1)
-        elif direction == "left" and self.x-1 >= 0:
-            return (self.x-1, self.y) 
-        elif direction == "right" and self.x+1 < GAME_WIDTH:
-            return (self.x+1, self.y)
-        return (self.x, self.y)
-
-    def get_inventory(self):
-        return self.inventory
-
-class Gem(GameElement):
-    IMAGE = "GreenGem"
+class GreenGem(GameElement):
+    IMAGE = "OrangeGem"
     SOLID = True
+    level_portal = 'Chest'
 
-    def interact(self, player, direction):
-        if direction == "up" and self.y-1 >= 0:
-            return (self.x, self.y-1)
-        elif direction == "down" and self.y+1 < GAME_HEIGHT:
-            return (self.x, self.y+1)
-        elif direction == "left" and self.x-1 >= 0:
-            return (self.x-1, self.y) 
-        elif direction == "right" and self.x+1 < GAME_WIDTH:
-            return (self.x+1, self.y)
-        return (self.x, self.y)
+    def level_up(self, x, y):
+        portal_location = CURRENT_LEVEL.chest_location
 
-class BlueGem(Gem):
-    IMAGE = "BlueGem"
+        if (x, y) == portal_location:
+            Level.current_level += 1
+            GAME_BOARD.draw_msg("You made it to level %d!" % Level.current_level)
+            next_level = CURRENT_LEVEL.make_next_level()
+            draw_board(next_level)
 
-    def interact(self):
-        draw_board(LEVEL2)
 
 class Chest(GameElement):
     IMAGE = "Chest"
     SOLID = True
 
-    def interact(self, player):
-        pass
+    def move_forward(self, direction):
+        return (self.x, self.y)
 
+class Person(GameElement):
+    IMAGE = "Boy"
+    SOLID = True
+
+    def interact(self, direction):
+        GAME_BOARD.draw_msg("Push the gem into the chest.")
+        return(self.x, self.y)
+
+    def move_forward(self, direction):
+        return (self.x, self.y)
+
+class Tree(GameElement):
+    IMAGE = "ShortTree"
+    SOLID = True
+
+    def move_forward(self, direction):
+        return (self.x, self.y)
+
+CHARACTERS = {
+    "Boy": Person,
+    "Gem": GreenGem,
+    "Chest": Chest,
+    "Tree": Tree,
+    "Rock": Rock,
+}
 ####   End class definitions    ####
 
 def initialize():
     """Put game initialization code here"""
     global PLAYER
     PLAYER = Character()
+    global CURRENT_LEVEL
+    CURRENT_LEVEL = Level()
     GAME_BOARD.register(PLAYER)
     GAME_BOARD.set_el(0, 0, PLAYER)
 
-    draw_board(LEVEL1)
+    level_one = CURRENT_LEVEL.make_next_level()
+    draw_board(level_one)
 
  
 def keyboard_handler():
@@ -127,23 +153,25 @@ def keyboard_handler():
         GAME_BOARD.erase_msg() 
 
     if direction:
-        next_location = PLAYER.next_pos(direction)
-        next_x = next_location[0]
-        next_y = next_location[1]      
+        next_player = PLAYER.move_forward(direction)
+        next_x = next_player[0]
+        next_y = next_player[1]      
 
         exisiting_el = GAME_BOARD.get_el(next_x, next_y)
 
         if exisiting_el:
-            next_direction = exisiting_el.interact(PLAYER, direction)
+            next_existing_el = exisiting_el.move_forward(direction)
             GAME_BOARD.del_el(exisiting_el.x, exisiting_el.y)
-            GAME_BOARD.set_el(next_direction[0], next_direction[1], exisiting_el)
+            GAME_BOARD.set_el(next_existing_el[0], next_existing_el[1], exisiting_el)
 
         if GAME_BOARD.get_el(next_x, next_y) is None or not exisiting_el.SOLID:
             GAME_BOARD.del_el(PLAYER.x, PLAYER.y)
             GAME_BOARD.set_el(next_x, next_y, PLAYER)
 
+        if exisiting_el:
+            exisiting_el.level_up(next_existing_el[0], next_existing_el[1])
 
-def draw_board(level):
+def draw_board(level_description):
     # clear the existing board
     for x in range(GAME_WIDTH):
             for y in range(GAME_HEIGHT):
@@ -151,21 +179,15 @@ def draw_board(level):
                     pass
                 else:
                     GAME_BOARD.del_el(x,y)
+
     # use the correct level dictionary to draw the new board
-    for k, v in level.iteritems():
+    for k, v in level_description.iteritems():
         if GAME_BOARD.get_el(k[0], k[1]) == PLAYER:
-            print k[0], k[1]
             pass
         elif v == '':
             pass
         else:
-            if v == 'BlueGem':
-                item = BlueGem()
-            elif v == 'Gem':
-                item = Gem()
-            elif v == 'Chest':
-                item = Chest()
+            item = CHARACTERS[v]()
             GAME_BOARD.register(item)
             GAME_BOARD.set_el(k[0], k[1], item)
-
 
